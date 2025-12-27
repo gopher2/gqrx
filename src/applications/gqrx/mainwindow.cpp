9661491b070e6022c3448a97b5e3df0b561ba730
gopher2 <davidkierz@gmail.com>
2025-12-27 11:03:11 -0500

    Disable frequency control during IQ file playback
    Frequency is fixed to the recorded file's center frequency, so prevent
    user from changing it during playback to avoid confusion.

--- src/applications/gqrx/mainwindow.cpp
+++ src/applications/gqrx/mainwindow.cpp
@@ -1978,6 +1978,9 @@ void MainWindow::startIqPlayback(const QString& filename, float samprate, qint64
         ui->statusBar->showMessage(tr("Playing %1").arg(filename));
     }
 
+    // Disable frequency control during playback (frequency is fixed to recorded file)
+    ui->freqCtrl->setEnabled(false);
+
     std::cout << "DEBUG: Starting DSP..." << std::endl;
     on_actionDSP_triggered(true);
     std::cout << "DEBUG: DSP started" << std::endl;
@@ -2074,6 +2077,9 @@ void MainWindow::stopIqPlayback()
         // restsart DSP
         on_actionDSP_triggered(true);
     }
+
+    // Re-enable frequency control now that we're back to live input
+    ui->freqCtrl->setEnabled(true);
 }
 
 
